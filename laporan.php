24ad4d182e42965599fafc6159c93de841924807
wahab oni <wahabthea@gmail.com>
2019-03-11 11:41:29 +0700

    -

---
+++ laporan.php
@@ -0,0 +1,52 @@
+<html lang="id">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+
+
+	<!-- Material Design Bootstrap -->
+	<link rel="icon" href="favicon.ico"/>
+	<title>Program Data Penjualan</title>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<h3>INPUT PENJUALAN</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+			</p>
+			<button class="btn btn-large btn-success">
+				<span class="fa fa-store-alt"></span>
+				Penjualan
+			</button>
+		</div>
+		<div class="col">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eveniet nemo ullam non totam dignissimos aliquam at recusandae, blanditiis repellat suscipit vel! Perspiciatis sit totam molestias, voluptatem voluptatibus aperiam officia.
+		</div>
+		<div class="col">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor nisi eveniet necessitatibus distinctio vitae asperiores tempora excepturi illum, repellendus assumenda pariatur earum aspernatur facere laborum voluptatibus laboriosam ullam ipsa obcaecati!
+		</div>
+		<div class="col">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores nobis, culpa laudantium unde. Beatae repudiandae sunt reprehenderit, sint, quasi in vero cum quas sed nisi eum ab corporis, laudantium provident?
+		</div>
+		<div class="col">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque facere animi, provident ab cum porro quam saepe, distinctio, deserunt perspiciatis quos! Nulla ut consectetur fuga beatae eum. Aspernatur saepe, soluta.
+		</div>
+	</div>
+
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
