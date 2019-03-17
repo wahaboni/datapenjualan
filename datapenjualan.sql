ea4b9e2fe87beda1df9cd13af5572d4bf9ba66e4
wahaboni <wahabthea@gmail.com>
2019-03-17 10:19:57 +0700

    Update datapenjualan.sql

--- datapenjualan.sql
+++ datapenjualan.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version 5.5.5-10.1.37-MariaDB)
-# Date: 2019-03-02 10:15:14
+# Date: 2019-03-17 10:19:38
 # Generator: MySQL-Front 6.1  (Build 1.26)
 
 
@@ -10,18 +10,19 @@
 DROP TABLE IF EXISTS `data_akun`;
 CREATE TABLE `data_akun` (
   `id_akun` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_akun` varchar(255) DEFAULT NULL,
+  `nama_akun` varchar(255) NOT NULL DEFAULT '',
   `ket_akun` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `hak_akses` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_akun`)
+  PRIMARY KEY (`id_akun`),
+  UNIQUE KEY `nama_akun` (`nama_akun`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1004 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "data_akun"
 #
 
-INSERT INTO `data_akun` VALUES (1001,'administrator','Administrator','admin789','1'),(1002,'gudang','Gudang','12345','2'),(1003,'itstore','IT Store - Karawaci','112233','3');
+INSERT INTO `data_akun` VALUES (1001,'administrasi','Administrasi','admin789','1'),(1002,'gudang','Gudang','12345','2'),(1003,'itstore','IT Store - Karawaci','112233','3');
 
 #
 # Structure for table "data_barang"
@@ -38,13 +39,30 @@ CREATE TABLE `data_barang` (
   `harga_m2` int(11) DEFAULT NULL,
   `komisi` int(11) DEFAULT NULL,
   PRIMARY KEY (`kode_barang`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "data_barang"
 #
 
-INSERT INTO `data_barang` VALUES (1,'Acer Nitro 5','i5-8250U, RAM 8GB SSD 25GB Windows',21,'Acer',8200000,8700000,80000),(2,'Legion Y520 67ID','i7 ram 8GB gtx 1050 4GB',10,'Lenovo',13500000,13899000,100000),(3,'Flex 5','RAM 8GB MX130 2GB Windows',22,'Lenovo',12900000,13000000,100000),(4,'Miix 320','Intel Atom, 4GB, emmc 128GB',8,'Lenovo',3100000,3500000,50000),(5,'Lenovo IP 330 - i5 Win VGA','i5 8250U 530 2GB RAM 4GB HDD 1TB Windows 10 Resmi',6,'Lenovo',8500000,8900000,100000),(6,'Asus X4004AU','i3 RAM 4GB HDD 500GB Windows resmi',4,'Asus',5900000,6300000,50000),(7,'AIO 300','i3-6100T RAM 4GB HDD 500GB DOS',0,'Lenovo',6900000,7200000,100000),(8,'Acer Aspire 3','Celeron RAM 4GB HDD 500GB',20,'Acer',3000000,3300000,0),(9,'Asus ROG GL552JX','GL552JX-XO139D',6,'Asus',8300000,8500000,100000);
+INSERT INTO `data_barang` VALUES (1,'Acer Nitro 5','i5-8250U, RAM 8GB SSD 25GB Windows',8,'Acer',8200000,8700000,80000),(2,'Legion Y520 67ID','i7 ram 8GB gtx 1050 4GB',7,'Lenovo',13500000,13899000,100000),(3,'Flex 5','RAM 8GB MX130 2GB Windows',16,'Lenovo',12900000,13000000,100000),(4,'Miix 320','Intel Atom, 4GB, emmc 128GB',5,'Lenovo',3100000,3500000,50000),(6,'Asus X4004AU','i3 RAM 4GB HDD 500GB Windows resmi',4,'Asus',5900000,6300000,50000),(8,'Acer Aspire 3','Celeron RAM 4GB HDD 500GB',17,'Acer',3000000,3300000,0),(9,'Asus ROG GL552JX','GL552JX-XO139D',6,'Asus',8300000,8500000,100000),(12,'ThinkPad X1 Carbon','i7 RAM 8GB HDD 1TB SSD 256GB Windows',6,'Lenovo',1700000,17600000,100000),(13,'laptop hp 14s-cf0044tx','core i5-8250u - ram 4gb - hdd 1tb - vga r520',6,'HP',7700000,7900000,50000);
+
+#
+# Structure for table "status_penjualan"
+#
+
+DROP TABLE IF EXISTS `status_penjualan`;
+CREATE TABLE `status_penjualan` (
+  `kode_status` int(11) NOT NULL,
+  `status_penjualan` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`kode_status`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "status_penjualan"
+#
+
+INSERT INTO `status_penjualan` VALUES (1,'Booking'),(2,'Terjual'),(3,'Sudah Terjual'),(4,'Di Return');
 
 #
 # Structure for table "data_penjualan"
@@ -53,21 +71,25 @@ INSERT INTO `data_barang` VALUES (1,'Acer Nitro 5','i5-8250U, RAM 8GB SSD 25GB W
 DROP TABLE IF EXISTS `data_penjualan`;
 CREATE TABLE `data_penjualan` (
   `kode_penjualan` int(11) NOT NULL AUTO_INCREMENT,
-  `kode_barang` int(11) NOT NULL DEFAULT '0',
+  `kode_barang` int(11) DEFAULT '0',
+  `nama_barang` varchar(255) DEFAULT NULL,
   `harga_penjualan` int(11) DEFAULT NULL,
+  `harga_modal` int(11) DEFAULT NULL,
+  `komisi` int(11) DEFAULT NULL,
   `jenis_penjualan` varchar(255) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
+  `kode_status` int(11) DEFAULT NULL,
   `tgl_penjualan` date DEFAULT NULL,
-  `id_akun` int(11) DEFAULT NULL,
+  `nama_akun` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`kode_penjualan`),
-  KEY `kode_barang` (`kode_barang`),
-  KEY `id_akun` (`id_akun`),
-  CONSTRAINT `data_penjualan_ibfk_1` FOREIGN KEY (`kode_barang`) REFERENCES `data_barang` (`kode_barang`),
-  CONSTRAINT `data_penjualan_ibfk_2` FOREIGN KEY (`id_akun`) REFERENCES `data_akun` (`id_akun`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  KEY `data_penjualan_ibfk_2` (`nama_akun`),
+  KEY `data_penjualan_ibfk_3` (`kode_status`),
+  CONSTRAINT `data_penjualan_ibfk_2` FOREIGN KEY (`nama_akun`) REFERENCES `data_akun` (`nama_akun`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `data_penjualan_ibfk_3` FOREIGN KEY (`kode_status`) REFERENCES `status_penjualan` (`kode_status`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "data_penjualan"
 #
 
-INSERT INTO `data_penjualan` VALUES (1,1,8500000,'Online',3,'2019-02-28',1001),(2,8,70000000,'Offline',1,'2019-02-27',1002),(3,8,3600000,'Offline',2,'2019-02-27',1002),(4,8,3500000,'Online',1,'2019-02-27',1002),(5,6,6500000,'Offline',1,'2019-02-27',1002);
+INSERT INTO `data_penjualan` VALUES (1,2,'Legion Y520 67ID',14000000,13500000,100000,'Offline',2,2,'2019-03-13','itstore'),(2,1,'Acer Nitro 5',9000000,8200000,80000,'Offline',2,2,'2019-03-13','itstore'),(3,11,'Anjing',1400000,1200000,10,'Offline',3,2,'2019-03-13','itstore'),(4,13,'like',3000,1000,0,'Offline',10,2,'2019-03-13','itstore'),(5,13,'like',5000,1000,0,'Offline',2,2,'2019-03-13','itstore'),(6,4,'Miix 320',4000000,3100000,50000,'Online',3,2,'2019-03-13','administrasi'),(7,13,'laptop hp 14s-cf0044tx',7800000,7700000,50000,'Online',2,2,'2019-03-14','itstore');
